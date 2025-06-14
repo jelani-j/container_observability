a327a2772453cad7a88ebae5acabff002a0c038b
jelani_j <jelnjoseph4@gmail.com>
2025-06-14 13:01:43 -0400

    created backend, need to import aws credentials into image

--- node_backend/main.js
+++ node_backend/main.js
@@ -1,7 +1,7 @@
 //Main file to refresh all run all news scripts
 import { tech_savedata } from "./tech-news.js";
-import { world_savedata } from "../mysql_database/world-news.js";
-import { travel_savedata } from "../mysql_database/travel-news.js";
+import { world_savedata } from "./world-news.js";
+import { travel_savedata } from "./travel-news.js";
 // this resets the entire json file and then refreshes it with new information
 async function main(){
     try{
